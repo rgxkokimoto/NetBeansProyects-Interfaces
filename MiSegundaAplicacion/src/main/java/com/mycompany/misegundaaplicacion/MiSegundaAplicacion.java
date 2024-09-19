68720e6170c5f6d4d20fa71e9daadce452933830
Alejandro <aaleex8842@gmail.com>
2024-09-19 09:59:34 +0200

    Primera subida de MIsegundaAplicacion

---
+++ MiSegundaAplicacion/src/main/java/com/mycompany/misegundaaplicacion/MiSegundaAplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.misegundaaplicacion;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MiSegundaAplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
