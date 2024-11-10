52e03748acbf9bfb5850f925a98cc5006e1ca347
Alejandro <aaleex8842@gmail.com>
2024-11-10 13:43:42 +0100

    preparando proyecto para estilos

---
+++ z_SytlesUI/src/app/ControlGeneral.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package app;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class ControlGeneral implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
